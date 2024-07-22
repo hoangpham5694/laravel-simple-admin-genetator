f94c47c368bb6427a6299d254f086f0ef0cdbd64
Minh Hoang <hoangpham5694@gmail.com>
2024-07-22 22:10:31 +0700

    - fix wrong destination AdminSeeder (#2)

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -27,8 +27,8 @@ class InstallCommand extends Command
         $this->info('Generating layout...');
         $this->createDirectory(resource_path('views/sag'));
         $this->copyDirectory(__DIR__.'/../../stubs/views', resource_path('views/sag'));
-//        $this->createDirectory(public_path('sag'));
-//        $this->copyDirectory(__DIR__.'/../../stubs/public', public_path('sag'));
+        $this->createDirectory(public_path('sag'));
+        $this->copyDirectory(__DIR__.'/../../stubs/public', public_path('sag'));
         file_put_contents(
             './routes/web.php',
             "\nRoute::middleware(['admin'])->group(function () {\n Route::get('/admin/dashboard', [\App\Http\Controllers\SAG\HomeController::class, 'dashboard'])->name('simple_admin_generation.dashboard');\n});\n",
@@ -36,7 +36,7 @@ class InstallCommand extends Command
         );
         Artisan::call('migrate');
 
-        Artisan::call('db:seed --class="HoangPham\\\SimpleAdminGeneration\\\Database\\\Seeders\\\AdminSeeder"');
+        Artisan::call('db:seed --class="HoangPhamDev\\\SimpleAdminGenerator\\\Database\\\Seeders\\\AdminSeeder"');
 
         $this->info('Done');
     }
